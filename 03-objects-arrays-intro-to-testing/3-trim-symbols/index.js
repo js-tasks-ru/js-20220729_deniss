262f6445dab768302d5d3b82a9c433e46d500f7c
Denis Solntsev <denis.solntsev@gmail.com>
2022-08-07 20:27:52 +0300

    Function trimSymbols() refactoring

--- 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
+++ 03-objects-arrays-intro-to-testing/3-trim-symbols/index.js
@@ -5,16 +5,11 @@
  * @returns {string} - the new string without extra symbols according passed size
  */
 export function trimSymbols(string, size) {
-  let duplicateCharCounter = 0;
-  if(size == 0) return '';  
-  if(size === undefined) size = string.length;
-
-  return [...string].reduce((trimmedStr, nextChar) => {
-    if(trimmedStr[trimmedStr.length - 1] === nextChar) {
-      return (duplicateCharCounter++ < size) ? trimmedStr += nextChar : trimmedStr;   
-    } else {
-      duplicateCharCounter = 1;        
-      return trimmedStr += nextChar;
-    }
+    if(size === 0) return '';  
+    if(size === undefined) size = string.length;
+  
+    return [...string].reduce((trimmedStr, nextChar) => {
+      if(!trimmedStr.endsWith(nextChar.repeat(size))) trimmedStr += nextChar;
+      return trimmedStr;
   }, '');
 }
