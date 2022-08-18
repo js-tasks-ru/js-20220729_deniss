f6ee9253c9104075384c9c39e8039179971766a0
Denis Solntsev <denis.solntsev@gmail.com>
2022-08-18 21:54:04 +0300

    Tooltp component implementation

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,7 +1,63 @@
-class Tooltip {
-  initialize () {
+export default class Tooltip {
+  static #instance;
+  offset = 15;
 
+  constructor() {
+    if(Tooltip.#instance) return Tooltip.#instance;
+    Tooltip.#instance = this;
   }
-}
 
-export default Tooltip;
+  onTooltipPointerOver = (event) => {
+    const elem = event.target.closest('[data-tooltip]');
+    if(!elem) return;
+
+    let tooltip = elem.dataset.tooltip;
+    this.render(tooltip);
+    document.addEventListener('pointermove', this.onTooltipPointerMove);
+  }
+
+  onTooltipPointerMove = (event) => {
+    this.element.style.left = `${event.pageX + this.offset}px`;
+    this.element.style.top = `${event.pageY + this.offset}px`;  
+  }
+
+  onTooltipPointerOut = (event) => {
+    const elem = event.target.closest('[data-tooltip]');
+    if(!elem) return;
+
+    if(Tooltip.#instance) {
+      this.remove();
+    }
+  }
+
+  initialize() {
+    document.addEventListener('pointerover', this.onTooltipPointerOver);
+    document.addEventListener('pointerout', this.onTooltipPointerOut);
+  }
+
+  getTemplate(text) {
+    return `<div class="tooltip">${text}</div>`
+  }
+
+  render(text) {
+    const element = document.createElement('div');
+    element.innerHTML = this.getTemplate(text);
+    this.element = element.firstElementChild;
+    document.body.append(this.element);
+  }
+
+  remove() {
+    if (this.element) {
+      document.removeEventListener('pointermove', this.onTooltipPointerMove);
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    document.removeEventListener('pointerover', this.onTooltipPointerOver);
+    document.removeEventListener('pointerout', this.onTooltipPointerOut);
+    this.remove();
+    Tooltip.#instance = null;
+    this.element = null;
+  } 
+}
